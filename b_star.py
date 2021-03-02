39080262eac999e8f95b639183bf1dcff6e7176f
Artsem Zhuk <zhuk199@gmail.com>
2021-03-02 10:10:49 +0300

    b_star for single

--- b_star.py
+++ b_star.py
@@ -9,7 +9,7 @@ def density(n, k):
     return st.binom.pmf(k, n, .5)
 
 
-def get_b_stars(graph: nx.Graph, adjusted=False):
+def get_degree_pairs(graph: nx.Graph):
     cnt = {}
     for u, v in graph.edges():
         du = graph.degree(u)
@@ -19,6 +19,11 @@ def get_b_stars(graph: nx.Graph, adjusted=False):
         if not (du, dv) in cnt:
             cnt[(du, dv)] = 0
         cnt[(du, dv)] += 1
+    return cnt
+
+
+def get_b_stars(graph: nx.Graph, adjusted=False):
+    cnt = get_degree_pairs(graph)
 
     weights = {}
     for (du, dv), freq in cnt.items():
@@ -38,15 +43,36 @@ def get_b_stars(graph: nx.Graph, adjusted=False):
     return weights
 
 
+def get_b_stars_single(graph: nx.Graph):
+    cnt = get_degree_pairs(graph)
+
+    weights = {}
+    for (du, dv), freq in cnt.items():
+        for av in range(0, dv):
+            for au in range(0, du):
+                for (bv, bu) in [(0, 1), (1, 0)]:
+                    den = bv - bu
+                    nom = ((av + bu) / dv - (au + bv) / du)
+                    if nom == 0:
+                        continue
+                    ratio = den / nom
+                    if ratio not in weights:
+                        weights[ratio] = 0
+                    weights[ratio] += freq
+    return weights
+
+
 if __name__ == '__main__':
-    # graph = GraphByDegrees({2: 5000, 3: 5000})
-    graph = ErdosRenyi(1000, 5 / 1000)
+    graph = GraphByDegrees({5: 5000})
+    # graph = GraphByDegrees({3: 5000, 5: 5000})
+    # graph = ErdosRenyi(1000, 5 / 1000)
     # graph = ScaleFree(100, 5)
-    ratios = get_b_stars(graph, adjusted=True)
+    ratios = get_b_stars_single(graph)
+    print(ratios)
     ratios = {k: v for k, v in ratios.items() if 1 < k <= 10}
-    # plt.hist(list(ratios.keys()), weights=list(ratios.values()), bins=100)
-    # plt.show()
-    # exit(0)
+    plt.hist(list(ratios.keys()), weights=list(ratios.values()), bins=100)
+    plt.show()
+    exit(0)
     xs = np.linspace(1, 10, 100)
 
     total = sum(ratios.values())
